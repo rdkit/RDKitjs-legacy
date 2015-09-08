c7128264f94fd79d8733fe5d7cadaf66619bd30d
thegodone <guillaume.godin@gmail.com>
2015-09-08 16:01:38 +0200

    repairing SA / NP scores

--- src/SAscore.js
+++ src/SAscore.js
@@ -1,3 +1,13 @@
+var dict2 = require ('./fpscores.pkldico.json');
+//console.log(Object.keys(dict2).length);
+var dict = require ('./publicnp.model.json');
+
+
+var RDKit = require ('rdkit');
+var R = RDKit;
+
+
+
 /*
 #
 # calculation of synthetic accessibility score as described in:
@@ -18,68 +28,72 @@
 # js adapatation by guillaume godin september 2015
 */
 
+function SAscore (dico, nAtoms,fplen, morganFPlist, molecule){
+	var nf = 0;
+	var score1=0;
+	var v=0;
+	var f=0;
+	for (i =0;i<fplen;i++){   
+		v = morganFPlist.get(i+fplen);
+		f = dico[morganFPlist.get(i)];
+		if (f !== undefined) {  
+			score1 = score1 + f*v;
+		}
+		// default value for undefined fragments!
+		else {
+			score1 = score1 -4*v;
+		} 
+		nf = nf + v;
+	}
 
-var dict2 = require ('./fpscores.pkldico.json');
+	score1=score1/nf;
 
-console.log(Object.keys(dict2).length);
+	//console.log(score1);
 
-var RDKit = require ('rdkit');
-var R = RDKit;
-m=R.Molecule.fromSmiles('Cc1ccccc1');
-var u =m.getMorganFPlist(2);
-var fplen =  u.size()/2;
-var nf = 0;
-var score1=0;
-var v=0;
-for (i =0;i<fplen;i++){   
-	v = u.get(i+fplen);
-	nf = nf + v;
-	score1 = score1 + dict2[u.get(i)]*v;
+	var p = molecule.FindMolChiralCenters(true);
+	var nChiralCenters = p.size();
+	var SBM = molecule.getSpiroBridgeMacrocycles(); // there is a issue there S
+	var nSpiro = SBM.get(0);
+	var nBridgeheads = SBM.get(1);
+	var nMacrocycles = SBM.get(2);
+	var sizePenalty = Math.pow(nAtoms,1.005) - nAtoms;
+	var stereoPenalty = Math.log10(nChiralCenters+1);
+	var spiroPenalty = Math.log10(nSpiro+1);
+	var bridgePenalty = Math.log10(nBridgeheads+1);
+	var macrocyclePenalty = 0;
+
+  	//console.log("1:",nBridgeheads,"2:",nMacrocycles,"3:",nSpiro,"4:",nChiralCenters,"5:",macrocyclePenalty,"6:",sizePenalty)
+
+
+	if (nMacrocycles > 0)
+	    macrocyclePenalty = math.log10(2);
+    
+	var score2 = 0-sizePenalty-stereoPenalty-spiroPenalty-bridgePenalty-macrocyclePenalty;
+	//console.log(score2);
+
+	var score3 = 0;
+	if (nAtoms > fplen)
+	    score3 = 0.5*Math.log(nAtoms/fplen);
+
+	var sascore = score1 + score2 + score3;
+	//console.log(sascore);
+
+	min = -4.0;
+	max = 2.5;
+
+	sascore = 11 - (sascore - min + 1) / (max - min) * 9;
+
+	if (sascore > 8) 
+	  	sascore = 8 + math.log(sascore-8);
+	if (sascore > 10)
+	   sascore = 10.0;
+	else if (sascore < 1)
+	   sascore = 1;
+	//console.log(sascore);
+
+	return sascore;
 }
-score1=score1/nf;
 
-console.log(score1);
-
-var nAtoms = m.getNumAtoms();
-var p = m.FindMolChiralCenters(true);
-var nChiralCenters = p.size();
-var SBM = m.getSpiroBridgeMacrocycles();
-var nBridgeheads = SBM.get(0);
-var nSpiro = SBM.get(1);
-var nMacrocycles = SBM.get(2);
-var sizePenalty = Math.pow(nAtoms,1.005) - nAtoms;
-var stereoPenalty = Math.log10(nChiralCenters+1);
-var spiroPenalty = Math.log10(nSpiro+1);
-var bridgePenalty = Math.log10(nBridgeheads+1);
-var macrocyclePenalty = 0;
-
-if (nMacrocycles > 0)
-    macrocyclePenalty = math.log10(2);
-
-var score2 = -sizePenalty-stereoPenalty-spiroPenalty-bridgePenalty-macrocyclePenalty;
-console.log(score2);
-
-var score3 = 0;
-if (nAtoms > fplen)
-    score3 = 0.5*Math.log(nAtoms/fplen);
-
-var sascore = score1 + score2 + score3;
-
-min = -4.0;
-max = 2.5;
-
-sascore = 11 - (sascore - min + 1) / (max - min) * 9;
-console.log(sascore);
-
-if (sascore > 8) 
-  	sascore = 8 + math.log(sascore-8);
-if (sascore > 10)
-   sascore = 10.0;
-else if (sascore < 1)
-   sascore = 1;
-
-console.log('SAscore:');
-console.log(sascore);
 
 /*
 #
@@ -97,24 +111,69 @@ console.log(sascore);
 # peter ertl, august 2015
 # javascript adaptation september 2015
 */
-var dict = require ('./publicnp.model.json');
+function NPscore(dicts, nAtoms,fplen,morganFPlist) {
+    var v=0;
+    var f=0;
+	var npscore = 0;
+	for (i =0;i<fplen;i++){   
+		v = morganFPlist.get(i+fplen);
+		f =dicts[morganFPlist.get(i)];
+		if (f !== undefined)
+			npscore = npscore + f;
+	}
+	npscore = npscore/nAtoms;
 
-npscore = 0;
-for (i =0;i<fplen;i++){   
-	v = u.get(i+fplen);
-	nf = nf + v;
-	npscore = npscore + dict[u.get(i)];
+	//# preventing score explosion for exotic molecules
+	if (npscore > 4) 
+	    npscore = 4 + Math.log10(npscore - 4 + 1);
+	if (npscore < -4)
+	    npscore = -4 - Math.log10(-4. -npscore + 1.);
+	return npscore;
 }
 
-npscore = npscore/nAtoms;
 
-//# preventing score explosion for exotic molecules
-if (npscore > 4) 
-    npscore = 4 + Math.log10(npscore - 4 + 1);
-if (npscore < -4)
-    npscore = -4 - Math.log10(-4. -npscore + 1.);
-console.log('NPscore:');
-console.log(npscore);
+//loop over file or list of smiles!
+/*
+var fs = require('fs');
+var np = 0;
+var sa = 0;
+fs.readFileSync('test-sardkit.txt').toString().split(/\r?\n/).forEach(function(line){
+    var linecomponent = line.split('\t'); 
+    //try {
+
+		var m=R.Molecule.fromSmiles(linecomponent[0]);
+		if (m !==undefined)
+		{
+		var morganFPlist =m.getMorganFPlist(2);
+		var nAtoms = m.getNumAtoms();
+		var fplen =  morganFPlist.size()/2;
+		sa = SAscore (dict2,nAtoms,fplen, morganFPlist,m);
+		np = NPscore(dict, nAtoms,fplen,morganFPlist);
+
+		console.log(linecomponent[1],";sa:",sa.toPrecision(4),";np:",np.toPrecision(4),";",linecomponent[0]);
+
+		m.delete();
+	}
+	/*}
+	catch (err) {
+            //console.log(linecomponent);
+
+	}
+
+});
+*/
+
+
+
+var m=R.Molecule.fromSmiles('CC(=O)OC1C[NH+]2CCC1CC2');
+var morganFPlist =m.getMorganFPlist(2);
+var nAtoms = m.getNumAtoms();
+var fplen =  morganFPlist.size()/2;
+sa = SAscore (dict2,nAtoms,fplen, morganFPlist,m);
+console.log("sa:",sa.toPrecision(4));
+
+
+
 
 
 
