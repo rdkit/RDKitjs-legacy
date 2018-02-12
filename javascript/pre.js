f724fc41355de6ca8b39d59c08dd7b2baa9792d9
Michaël Zasso <targos@protonmail.com>
2018-02-12 16:42:07 +0100

    start fixing wasm build

--- javascript/pre.js
+++ javascript/pre.js
@@ -1,12 +1,57 @@
 (function(factory){
+  function getCurrentPathBrowser() {
+    sc = document.getElementsByTagName("script");
+
+    for (idx = 0; idx < sc.length; idx++) {
+      s = sc.item(idx);
+
+      if (s.src && s.src.match(/rdkit\.js$/)) { return s.src; }
+    }
+  }
+  function getModuleForBrowser() {
+    var resolve;
+    var promise = new Promise((res, rej) => {
+      resolve = res;
+    });
+    var path = getCurrentPathBrowser();
+    var Module = {
+      locateFile(url) {
+        return new URL(url, path).href;
+      },
+      onRuntimeInitialized() {
+        resolve();
+      },
+      then: promise.then.bind(promise)
+    }
+    return Module;
+  }
+  function getModuleForNode() {
+    var resolve;
+    var promise = new Promise((res, rej) => {
+      resolve = res;
+    });
+    var Module = {
+      locateFile(url) {
+        return __dirname + '/' + url;
+      },
+      onRuntimeInitialized() {
+        resolve();
+      },
+      then: promise.then.bind(promise)
+    }
+    return Module
+  }
   if (typeof define === 'function' && define.amd) {
     define(function(){
-      return factory();
+    const Module = getModuleForBrowser();      
+      return factory(Module);
     });
   } else if (typeof module === "object" && module.exports) {
-    module.exports = factory();
+    const Module = getModuleForNode();
+    module.exports = factory(Module);
   } else {
+    const Module = getModuleForBrowser();
     window.RDKit = factory();
   }
-})(function(){
+})(function(Module){
 
