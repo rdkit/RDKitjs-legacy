34bff088236a3a4f289d145dd28d97453aad11e2
Michaël Zasso <targos@protonmail.com>
2018-02-13 11:08:16 +0100

    add benchmark

---
+++ benchmark/fromSmiles.js
@@ -0,0 +1,21 @@
+var rdkit = require('.');
+
+rdkit.load().then(() => {
+
+function test() {
+  for (var i = 0; i < 100; i++) {
+    rdkit.Molecule.fromSmiles('CCOOCCOCOCOCOCCOOCCCCOC');
+  }
+}
+
+test();
+
+console.time('test');
+test();
+test();
+test();
+test();
+test();
+console.timeEnd('test');
+
+});
