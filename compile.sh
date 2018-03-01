c24ca70946f3cb6630fbb25a76a5257d809acdfb
thegodone <guillaume.godin@gmail.com>
2018-03-01 13:24:27 +0100

    adding addHs, Embed, MMFFopt

--- compile.sh
+++ compile.sh
@@ -7,5 +7,11 @@
 # -DTHREADS_PTHREAD_ARG=OFF
 #-DEIGEN3_INCLUDE_DIR=/path/to/eigen/3.3.4/include/eigen3/ \
 
+alias emcc=/Users/GVALMTGG/Github/emsdk/emscripten/1.37.33/emcc
+
 mkdir -p out
-emcc --bind -Os -s WASM=1 -s NODEJS_CATCH_EXIT=0 -s MODULARIZE=1 -s EXPORT_NAME='"'rdk'"' -o out/rdkit.js -Iincludes -Iincludes/rdkit includes/rdkit-lib/libRDKitSmilesParse.so includes/rdkit-lib/libRDKitGraphMol.so includes/rdkit-lib/libRDKitRDGeneral.so includes/rdkit-lib/libRDKitFileParsers.so rdkit.cc
+emcc --bind -Os -s WASM=1 -s NODEJS_CATCH_EXIT=0 -s MODULARIZE=1 -s EXPORT_NAME='"'rdk'"' -o out/rdkit.js -Iincludes -Iincludes/rdkit includes/rdkit-lib/libRDKitSmilesParse.so includes/rdkit-lib/libRDKitGraphMol.so \
+           includes/rdkit-lib/libRDKitRDGeneral.so includes/rdkit-lib/libRDKitFileParsers.so includes/rdkit-lib/libRDKitDistGeometry.so includes/rdkit-lib/libRDKitForceFieldHelpers.so \
+           includes/rdkit-lib/libRDKitRDGeometryLib.so includes/rdkit-lib/libRDKitDistGeomHelpers.so includes/rdkit-lib/libRDKitForceField.so \
+           includes/rdkit-lib/libRDKitEigenSolvers.so includes/rdkit-lib/libRDKitAlignment.so includes/rdkit-lib/libRDKitSubstructMatch.so \
+           rdkit.cc
--- rdkit.cc
+++ rdkit.cc
@@ -7,6 +7,16 @@
 #include <GraphMol/SmilesParse/SmilesParse.h>
 #include <GraphMol/ROMol.h>
 #include <GraphMol/RWMol.h>
+#include <GraphMol/MolOps.h>
+// RDkit 3D
+#include <GraphMol/DistGeomHelpers/Embedder.h>
+// RDkit ForceField
+// comments thegodone & Paolo => MMFF.h and Builder.h need to be patch to avoid class issues! 13_05_2015
+#include <GraphMol/ForceFieldHelpers/MMFF/MMFF.h>
+#include <GraphMol/ForceFieldHelpers/MMFF/Builder.h>
+#include <GraphMol/ForceFieldHelpers/MMFF/AtomTyper.h>
+#include <GraphMol/ForceFieldHelpers/UFF/UFF.h>
+#include <ForceField/MMFF/Params.h>
 
 using emscripten::allow_raw_pointers;
 using emscripten::class_;
@@ -30,9 +40,33 @@ std::string molToMolfile2D(RWMol *mol)
   return ss.str();
 }
 
+
+void addHs(RWMol mol)
+{
+    return RDKit::MolOps::addHs(mol);
+}
+
+
+int EmbedMolecule(ROMol mol, unsigned int maxIterations, int seed)
+{   
+    return RDKit::DGeomHelpers::EmbedMolecule(mol,maxIterations,seed);
+}
+
+std::vector<double> MMFFoptimizeMolecule(ROMol mol)
+{
+    std::vector<double> res(2);
+    std::pair<int, double> p = RDKit::MMFF::MMFFOptimizeMolecule(mol);
+    res[0] = static_cast<double>(p.first);
+    res[1] = p.second;
+    return res;
+}
+
 EMSCRIPTEN_BINDINGS(module)
 {
   class_<RWMol>("RWMol");
   function("smilesToMol", &smilesToMol, allow_raw_pointers());
   function("molToMolfile2D", &molToMolfile2D, allow_raw_pointers());
+  function("addHs", &addHs, allow_raw_pointers());
+  function("EmbedMolecule", &EmbedMolecule, allow_raw_pointers());
+  function("MMFFoptimizeMolecule", &MMFFoptimizeMolecule, allow_raw_pointers());
 }
