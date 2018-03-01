9159de0a1ffd9125036c483d3819b3544e994ed7
Michaël Zasso <targos@protonmail.com>
2018-03-01 15:33:40 +0100

    remove build directory

--- build/src/CMakeFiles/rdkitjs.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rdkitjs"
-  "rdkit.js"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rdkitjs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
