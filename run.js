a0e95c9011b298ca72c5ab98e4bcfc242a1aa6a3
Michaël Zasso <targos@protonmail.com>
2018-03-01 16:36:06 +0100

    fix to 3d

--- run.js
+++ run.js
@@ -2,30 +2,28 @@
 
 const lib = require('./lib/index');
 
-lib.then((rdkit) => {
-  console.log(rdkit);
-  console.log(rdkit.smilesToMolfile('COCO'));
+lib
+  .then((rdkit) => {
+    console.log(rdkit);
+    console.log(rdkit.smilesToMolfile('COCO'));
 
-  const mol = rdkit.Molecule.fromSmiles('COCO');
-  console.log(mol.toMolfile());
+    const mol = rdkit.Molecule.fromSmiles('COCO');
+    console.log(mol.toMolfile());
 
-  console.log('ici');
-  console.log(rdkit.Molecule.smilesTo3D('COCO').toMolfile());
+    console.log('ici');
+    console.log(rdkit.smilesTo3D('COCO').toMolfile());
 
-  console.log(mol.addHs());
-  console.log('la');
+    console.log(mol.addHs());
+    console.log('la');
 
-  console.log(mol.EmbedMolecule());
-  console.log('encore la');
+    console.log(mol.EmbedMolecule());
+    console.log('encore la');
 
-  console.log(mol.MMFFoptimizeMolecule());
-  console.log('enfin la');
+    console.log(mol.MMFFoptimizeMolecule());
+    console.log('enfin la');
 
-  console.log(mol.toMolfile());
+    console.log(mol.toMolfile());
 
-
-   
-
-  mol.delete();
-
-}).catch(console.error);
+    mol.delete();
+  })
+  .catch(console.error);
